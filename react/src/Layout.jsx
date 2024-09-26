4cdbd77b1757d132d94903ab82d4ec7722e965b4
brajesh708 <brajeshmewada694@gmail.com>
2024-09-26 12:09:05 +0530

    navbar

--- react/src/Layout.jsx
+++ react/src/Layout.jsx
@@ -1,13 +1,11 @@
 
 import { Link,Outlet } from "react-router-dom"
-import Home from "./components/Home"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 
 const Layout = () => {
   return (
     <>
-    <Link to={Navbar}></Link>
 
 <Outlet/>
 
